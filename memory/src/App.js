c261962ed5f2c5479b8eb964b7fccb020ecb557f
jgohbb <55856950+jgohbb@users.noreply.github.com>
2020-03-09 00:43:01 -0600

    some changes to syntax

--- memory/src/App.js
+++ memory/src/App.js
@@ -12,41 +12,38 @@ const footStyle = {
 
 class App extends Component {
   state = {
-    // Setting this.state.starWarsList to the avengers json array
+    // Setting this.state.starWarsList
     starWarsList,
-    clickedAvengerIds: [],
+    clickedIds: [],
     score: 0,
     topScore: 0,
     message: "Click an image to begin!",
     animate: "animated swing"
   };
 
-  //Main logic for the GAME score & reshuffling pictures
   handleClickPicture = id => {
-    // Arrange the pictures in a random manner
     let shuffledArray = this.handleShuffleArray(starWarsList);
     this.setState({ starWarsList: shuffledArray });
 
     //Check if the image is clicked twice
-    if (this.state.clickedAvengerIds.includes(id)) {
-      // this.state.message = 'You guessed incorrectly! ';
+    if (this.state.clickedIds.includes(id)) {
       console.log("Game Over reset values ");
       this.setState({
-        clickedAvengerIds: [],
+        clickedIds: [],
         score: 0,
-        message: "You guessed incorrectly! "
+        message: "You guessed wrong..use the FORCE and try again. "
       });
       return;
     } else {
       //Update the state with updated values
       this.setState({
         //Add clicked picture to the array
-        clickedAvengerIds: this.state.clickedAvengerIds.concat([id]),
+        clickedIds: this.state.clickedIds.concat([id]),
         //Increment Score
         score: this.state.score + 1,
         // topScore: this.state.score + 1,
         //Display Message
-        message: "You guessed it correctly"
+        message: "You guessed right..the FORCE is strong with this one."
       });
       console.log("Score", this.state.score);
       console.log("TopScore", this.state.topScore);
@@ -57,23 +54,21 @@ class App extends Component {
         this.handleShuffleArray(starWarsList);
         this.setState({ starWarsList: shuffledArray });
 
-        //Reset th Game & values
+        //Reset Game
         this.setState({
           topScore: this.state.score + 1,
-          message: "Well done my Padawan! Click on any imae to restart game.",
-          //Reset the Game
+          message: "Well done my Padawan! Click on any image to restart game.",
           score: 0,
-          clickedAvengerIds: []
-          // message: "Click an image to begin!"
+          clickedIds: []
         });
       }
-      // set topscore = score if score>topscore.
+      // set topscore
       else if (this.state.score + 1 > this.state.topScore) {
         this.setState({ topScore: this.state.score + 1 });
       }
     }
   };
-  //Function to shuffle opictures when clicked
+  //shuffle pictures when clicked
   handleShuffleArray = starWarsList => {
     for (let i = starWarsList.length - 1; i > 0; i--) {
       let j = Math.floor(Math.random() * (i + 1)); // random index from 0 to i
@@ -82,7 +77,6 @@ class App extends Component {
     return starWarsList;
   };
 
-  // Map over this.state.friends and render a FriendCard component for each friend object
   render() {
     return (
       <div>
@@ -97,16 +91,15 @@ class App extends Component {
           className="container-fluid p-2 col-9 justify-content-center"
         >
           <h3 className="text-center text-danger">
-            Try not to click the same image twice!
+            Do not click the same image twice!
           </h3>
           {/* Loop through all the items in the static list  */}
-          {this.state.starWarsList.map(avenger => (
+          {this.state.starWarsList.map(character => (
             <MemoryCard
-              id={avenger.id}
-              key={avenger.id}
-              name={avenger.name}
-              image={avenger.image}
-              // onclick call the handle event to calculate score & shuffle array
+              id={character.id}
+              key={character.id}
+              name={character.name}
+              image={character.image}
               clickPicture={this.handleClickPicture}
               animate={!this.state.score && this.state.topScore}
             />
@@ -115,7 +108,7 @@ class App extends Component {
         <footer style={footStyle}>
           <center className="p-3">
             <a
-              href="https://github.com/NVK2016/React-Clicky-Game"
+              href="https://github.com/jgohbb/iClicky"
               className="text-light"
               target="_blank"
               rel="noopener noreferrer"
--- memory/src/components/Cards/index.js
+++ memory/src/components/Cards/index.js
@@ -3,9 +3,8 @@ import "./style.css";
 
 function Card(props) {
   return (
-    //in case the value is incorrect wobble the icons
     <img
-      className={`rounded-circle m-3 hover11 column  ${
+      className={`img-thumbnail m-3 hover11 column  ${
         props.animate ? "animated wobble" : ""
       }`}
       type="button"
